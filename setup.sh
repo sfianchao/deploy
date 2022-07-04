20e24b4962c3703cc2dcbfee1c1bf1e68817d32f
sfianchao <30313481+sfianchao@users.noreply.github.com>
2022-07-05 01:05:51 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ echo "\nStep 1. compile solidity file\n"
 sudo solc Status.sol --bin --abi --optimize --overwrite -o ./
 
 echo "\nStep 2. web3j convert to java class\n"
-web3j generate solidity -b ./Status.bin -a ./Status.abi -o ./src/main/java -p idsl.crosschain.transfer.contract
+web3j generate solidity -b ./Status.bin -a ./Status.abi -o ./src/main/java -p idsl.crosschain.deploy.contract
 
